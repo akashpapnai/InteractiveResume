d5dcc78c539ab930c2dcf7a34c726791781b19ee
Akash Papnai <akashpapnai@gmail.com>
2023-07-12 19:18:48 +0530

    changes

--- src/Routes/Experience.js
+++ src/Routes/Experience.js
@@ -6,19 +6,31 @@ export default function Experience() {
          <div className="row row-cols-1 row-cols-md-3 g-4 d-flex justify-content-center align-items-center">
             <div className="col hover-zoom">
                <div className="card h-100">
-               <img src={process.env.PUBLIC_URL + '/Experience/ontrack.png'} className="card-img-top img-fluid" alt="Image 1"/>
+               <img src={process.env.PUBLIC_URL + '/Experience/ontrack.png'} className="card-img-top img-fluid" alt="Ontrack"/>
                <div className="card-body">
                   <h5 className="card-title"><span>Ontrack (Client: Netmeds)</span></h5>
                   <p className="card-text">Software Developer</p>
+                  <p className="card-text"><a target="_blank" href="https://www.ontrackhrs.com/">https://www.ontrackhrs.com/</a></p>
                </div>
                </div>
             </div>
             <div className="col hover-zoom">
                <div className="card h-100">
-               <img src={process.env.PUBLIC_URL + '/Experience/Netmeds.jpg'} className="card-img-top img-fluid" alt="Image 2"/>
+               <img src={process.env.PUBLIC_URL + '/Experience/Netmeds.jpg'} className="card-img-top img-fluid" alt="Netmeds"/>
                <div className="card-body">
                   <h5 className="card-title"><span>Netmeds</span></h5>
                   <p className="card-text">Software Developer</p>
+                  <p className="card-text"><a target="_blank" href="https://www.netmeds.com/">https://www.netmeds.com/</a></p>
+               </div>
+               </div>
+            </div>
+            <div className="col hover-zoom">
+               <div className="card h-100">
+               <img src={process.env.PUBLIC_URL + '/Experience/thegemini.png'} className="card-img-top img-fluid" alt="The Gemini"/>
+               <div className="card-body">
+                  <h5 className="card-title"><span>The Gemini</span></h5>
+                  <p className="card-text">Software Developer</p>
+                  <p className="card-text"><a target="_blank" href="https://www.thegemini.co.in">https://www.thegemini.co.in</a></p>
                </div>
                </div>
             </div>

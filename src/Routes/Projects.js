331a0b3b35e05a02d969097204bb72029855ef32
Akash Papnai <akashpapnai@gmail.com>
2024-01-29 23:32:28 +0530

    changes

--- src/Routes/Projects.js
+++ src/Routes/Projects.js
@@ -6,8 +6,8 @@ export default function Projects() {
          <h1 className="text-center">Projects</h1>
          <hr/>
          <h2 className="d-flex justify-content-center align-items-center">Web Projects</h2>
-         <div className="row row-cols-1 row-cols-md-6 g-4 d-flex justify-content-center align-items-center"> 
-            <div className="col hover-zoom d-flex justify-content-center align-items-center" style={{minWidth:'200px'}}>
+         <div className="row row-cols-1 row-cols-md-3 g-4 d-flex justify-content-center align-items-center">
+            <div className="col hover-zoom">
                <div className="card h-100">
                <a href='https://akashpapnai.bsite.net/' target='_blank'>
                   <img src={process.env.PUBLIC_URL + '/Projects/hospital.png'} className="card-img-top img-fluid p-4" alt="Practice Project Representing Hospital Business" />

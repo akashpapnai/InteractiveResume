8d9d5bec72387ade16945427ac7f4735291f667c
Akash Papnai <akashpapnai@gmail.com>
2024-05-23 18:57:41 +0530

    changes

--- src/Routes/Projects.js
+++ src/Routes/Projects.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 
 export default function Projects() {
    return (
@@ -9,13 +9,12 @@ export default function Projects() {
          <div className="row row-cols-1 row-cols-md-3 g-4 d-flex justify-content-center align-items-center">
             <div className="col hover-zoom">
                <div className="card h-100">
-               <a href='https://akashpapnai.bsite.net/' target='_blank'>
+               <a href='https://akashpapnai.github.io/AngularPracticeProject/browser' target='_blank' rel='noreferrer'>
                   <img src={process.env.PUBLIC_URL + '/Projects/hospital.png'} className="card-img-top img-fluid p-4" alt="Practice Project Representing Hospital Business" />
                </a>
                <div className="card-body">
                   <h5 className="card-title"><span>Hospital Business</span></h5>
-                  <p className="card-text">Asp.Net Practice Project</p>
-                  <p className='card-text'>Use Username: <b>userrandom</b> and Password: <b>123456789</b></p>
+                  <p className="card-text">Asp.Net and Angular Practice Project</p>
                </div>
                </div>
             </div>
